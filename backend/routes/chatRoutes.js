f7fbcc9962ec381540553a5a11bcf09870515648
mahatopriya <mahatopriya>
2026-04-08 15:46:46 +0530

    mongodb url changed

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { chatWithAI } = require("../controllers/chatController");
-
-// POST /api/chat
-router.post("/", chatWithAI);
-
-module.exports = router;
